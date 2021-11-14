1ac773abdd37b77b54fd1cbc87e2b12cfc7a30c0
brajesh030 <brajeshp030@gmail.com>
2021-11-14 11:37:04 +0530

    first commit

---
+++ Zomato/Zomato/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zomato.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
